e5ccd633eab8264a54b9d3b5a435fe08b9411d3d
Mosi <mostafa.semnani.74@gmail.com>
2026-07-03 11:22:15 +0330

    init java spring boot 4 monolithic banking systemt project

---
+++ banking-system/java/monolithic/src/main/java/ir/mostafa/semnani/banking_system/BankingSystemApplication.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.banking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemApplication.class, args);
+	}
+
+}
---
+++ banking-system/java/monolithic/src/test/java/ir/mostafa/semnani/banking_system/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
